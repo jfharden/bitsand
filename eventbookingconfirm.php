42333b6b1e769b19c5264771df40296ea1e9ee61
PeteAUK <pete.allison@gompels.co.uk>
2015-05-29 11:27:25 +0100

    Changed line endings from Mac endings to normal CF's

--- eventbookingconfirm.php
+++ eventbookingconfirm.php
@@ -1,1 +1,210 @@
-<?php/*Bitsand - a web-based booking system for LRP eventsCopyright (C) 2006 - 2014 The Bitsand Project (http://bitsand.googlecode.com/)This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/include ('inc/inc_head_db.php');include ('inc/inc_head_html.php');include ('inc/inc_paypalbutton.php');?><h1><?php echo TITLE?></h1><script type='text/javascript'>function updatePaypalButton(){	$('#txtAnotherAmount').attr('value',Number($('#txtAnotherAmount').attr('value')).toFixed(2));	if ($('#txtAnotherAmount').attr('value') == "NaN") { $('#txtAnotherAmount').attr('value', "0.00");}	$('#anotheramount').attr('value', $('#txtAnotherAmount').attr('value'));}</script><?php	if ((int)$_POST['eventid'] != 0)	{		$eventid = htmlentities(stripslashes($_POST['eventid']));		$sql = "select * from {$db_prefix}events where evEventID = $eventid";		$result = ba_db_query ($link, $sql);		$eventinfo = ba_db_fetch_assoc ($result);		echo "<h2>Your booking has been recorded - ".htmlentities (stripslashes ($eventinfo['evEventName']))."</h2>";	}	else	{		$sMsg = "The selected event is not currently open for bookings";		$sURL = fnSystemURL () . 'start.php?warn=' . urlencode ($sMsg);		header ("Location: $sURL");	}	$bookas = htmlentities(stripslashes($_POST['cboBookAs']));	$sql = "SELECT * from {$db_prefix}bookings where bkPlayerID = $PLAYER_ID and bkEventID = $eventid";	$result = ba_db_query ($link, $sql);	if (ba_db_num_rows($result) == 0)	{		$sql = "INSERT INTO {$db_prefix}bookings (bkPlayerID, bkEventID, bkBookAs, bkDateICConfirmed, bkDateOOCConfirmed) VALUES ($PLAYER_ID, $eventid, '$bookas', '$today', '$today')";		ba_db_query ($link, $sql);		$sql = "SELECT * from {$db_prefix}bookings where bkPlayerID = $PLAYER_ID and bkEventID = $eventid";		$result = ba_db_query ($link, $sql);		$bookinginfo = ba_db_fetch_assoc ($result);	}	else	{		$bookinginfo = ba_db_fetch_assoc ($result);	}	$sql = "delete from {$db_prefix}bookingitems where biBookingID = ".$bookinginfo['bkID'];	ba_db_query ($link, $sql);	echo "<table>";	echo "<tr><th>Description</th><th>Quantity</th><th>Cost</th></tr>";	$bookingtotal = 0;	$playerbooking = 0;foreach($_POST as $itemname => $value){	$itemname = htmlentities(stripslashes($itemname));	$value = htmlentities(stripslashes($value));	if (strpos($itemname, 'chk') !== false || strpos($itemname, 'cbo') !== false)	{		$itemid = str_replace('chk', '', $itemname);		$itemid = str_replace('cbo', '', $itemid);		$itemid = (int)$itemid;		$value = str_replace('on', '1', $value);		if ($itemid > 0)		{			$sql = "select * from {$db_prefix}items where itItemID = $itemid";			$result = ba_db_query ($link, $sql);			$iteminfo = ba_db_fetch_assoc ($result);			if ($value > 0)			{				echo "<tr><td>".$iteminfo['itDescription']."</td><td>$value</td><td>&pound;".$iteminfo['itItemCost']."</td></tr>";				$bookingtotal += ($iteminfo['itItemCost'] * $value);				//Create item record				$sql = "INSERT INTO {$db_prefix}bookingitems (biBookingID, biItemID, biQuantity) VALUES (".$bookinginfo['bkID'].", $itemid, $value)";				ba_db_query ($link, $sql);				//Update bookinginfo				if ($iteminfo['itBunk']) { $bookinginfo['bkBunkRequested'] = 1; }				if ($iteminfo['itMeal']) { $bookinginfo['bkMealTicket'] = 1; }				if ($iteminfo['itAvailability'] == "Player") { $playerbooking = 1; }			}		}	}}	$bookingtotal = number_format($bookingtotal, 2);	$bookinginfo['bkAmountExpected'] = $bookingtotal;	echo "<tr class='total'><td colspan=2 class='totallabel'>Total:</td><td>&pound;$bookingtotal</span></td></tr>";	echo "</table>";	//Deal with booking queue people	$bookinginfo['bkInQueue'] = 0;	if ($eventinfo['evUseQueue'])	{		$sql = "select chFaction from {$db_prefix}characters where chPlayerID = $PLAYER_ID";		$result = ba_db_query ($link, $sql);		$characterinfo = ba_db_fetch_assoc ($result);		if ($playerbooking == 1 && $characterinfo['chFaction'] != DEFAULT_FACTION)		{			$bookinginfo['bkInQueue'] = 1;			$queuereason = "your character is not a member of the default faction.";		}		//Deal with being over the limit		if (QUEUE_OVER_LIMIT)		{			if ($bookinginfo['bkBookAs'] == "Player") { $spaces = $eventinfo['evPlayerSpaces']; }			if ($bookinginfo['bkBookAs'] == "Monster") { $spaces = $eventinfo['evMonsterSpaces']; }			if ($bookinginfo['bkBookAs'] == "Staff") { $spaces = $eventinfo['evStaffSpaces']; }			$limitsql = "select count(bkID) as BookingCount from {$db_prefix}bookings where bkInQueue = 0 and bkBookAs ='".$bookinginfo['bkBookAs']."' and bkEventID = $eventid ";			$limitresult = ba_db_query ($link, $limitsql);			$BookingCount = ba_db_fetch_assoc ($limitresult);			$BookingCount = $BookingCount['BookingCount'];			if ($BookingCount > $spaces)			{				$bookinginfo['bkInQueue'] = 1;				$queuereason = "there are no spaces remaining of your booking type.";			}		}	}	if ($bookinginfo['bkInQueue'] == 0)	{		if ($bookingtotal > 0)		{			echo "<table class='payment'>";			echo "<tr><td>Pay Later</td><td><a href='start.php'>Pay later</a></td></tr>";			if (USE_PAY_PAL) {				echo "<tr><td>Pay balance of &pound;$bookingtotal via Paypal:</td><td>";				generatePaypalButton("Event booking - ".$bookinginfo['evEventName'] . " (" . PID_PREFIX . sprintf ('%03s', $PLAYER_ID).")", $PLAYER_ID, $bookingtotal, $bookinginfo['bkID']);				echo "</td></tr>";				echo "<tr><td>Pay another amount:</td><td><input type='text' value='".$bookingtotal."' onChange='updatePaypalButton()' id='txtAnotherAmount' class='paypalamount'/>";				generatePaypalButton("Event booking - ".$bookinginfo['evEventName'] . " (" . PID_PREFIX . sprintf ('%03s', $PLAYER_ID).") - Partial Payment", $PLAYER_ID, $bookingtotal, $bookinginfo['bkID'], 'anotheramount');				echo "</td></tr>";			}			echo "<tr><td>Request payment by another user:</td><td>";			echo "<form action='start.php' method='post'><input type=hidden name='hBooking' value='".$bookinginfo['bkID']."' /><input type=text name='txtEmail'/><input type='submit' name='btnSubmit' value='Send Email' /></form>";			echo "</td></tr>";			echo "</table>";		}		else		{			echo "<p>Your booking is now complete, as it does not require any payment</p>";			$bookinginfo['bkDatePaymentConfirmed'] = $today;		}	}	else	{		echo "<p>Your booking has been placed in a queue because $queuereason</p>";		echo "<p>You will be able to pay once it is removed from the queue by an admin.</p>";		fnMailer ("Event booking - ".$eventinfo['evEventName'] . " (" . PID_PREFIX . sprintf ('%03s', $PLAYER_ID).")\nA booking has been placed in the queue", false);	}	//Save booking info	$sql = "UPDATE {$db_prefix}bookings SET ";	foreach ($bookinginfo as $key => $value)	{		if (!is_numeric($value)) {$value = "'".$value."'"; }		$sql.= "$key = $value,";	}	$sql = substr($sql,0,-1);	$sql.= " WHERE bkID = ".$bookinginfo['bkID'];	$result = ba_db_query ($link, $sql);include ('inc/inc_foot.php');?>
+<?php
+/*
+Bitsand - a web-based booking system for LRP events
+Copyright (C) 2006 - 2014 The Bitsand Project (http://bitsand.googlecode.com/)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+include ('inc/inc_head_db.php');
+
+include ('inc/inc_head_html.php');
+
+include ('inc/inc_paypalbutton.php');
+?>
+
+<h1><?php echo TITLE?></h1>
+
+<script type='text/javascript'>
+
+function updatePaypalButton()
+{
+	$('#txtAnotherAmount').attr('value',Number($('#txtAnotherAmount').attr('value')).toFixed(2));
+	if ($('#txtAnotherAmount').attr('value') == "NaN") { $('#txtAnotherAmount').attr('value', "0.00");}
+	$('#anotheramount').attr('value', $('#txtAnotherAmount').attr('value'));
+
+}
+
+</script>
+<?php
+
+	if ((int)$_POST['eventid'] != 0)
+	{
+		$eventid = htmlentities(stripslashes($_POST['eventid']));
+		$sql = "select * from {$db_prefix}events where evEventID = $eventid";
+		$result = ba_db_query ($link, $sql);
+		$eventinfo = ba_db_fetch_assoc ($result);
+		echo "<h2>Your booking has been recorded - ".htmlentities (stripslashes ($eventinfo['evEventName']))."</h2>";
+	}
+	else
+	{
+		$sMsg = "The selected event is not currently open for bookings";
+		$sURL = fnSystemURL () . 'start.php?warn=' . urlencode ($sMsg);
+		header ("Location: $sURL");
+	}
+
+	$bookas = htmlentities(stripslashes($_POST['cboBookAs']));
+	$sql = "SELECT * from {$db_prefix}bookings where bkPlayerID = $PLAYER_ID and bkEventID = $eventid";
+
+	$result = ba_db_query ($link, $sql);
+
+	if (ba_db_num_rows($result) == 0)
+	{
+		$sql = "INSERT INTO {$db_prefix}bookings (bkPlayerID, bkEventID, bkBookAs, bkDateICConfirmed, bkDateOOCConfirmed) VALUES ($PLAYER_ID, $eventid, '$bookas', '$today', '$today')";
+		ba_db_query ($link, $sql);
+
+		$sql = "SELECT * from {$db_prefix}bookings where bkPlayerID = $PLAYER_ID and bkEventID = $eventid";
+		$result = ba_db_query ($link, $sql);
+		$bookinginfo = ba_db_fetch_assoc ($result);
+	}
+	else
+	{
+		$bookinginfo = ba_db_fetch_assoc ($result);
+
+	}
+
+	$sql = "delete from {$db_prefix}bookingitems where biBookingID = ".$bookinginfo['bkID'];
+	ba_db_query ($link, $sql);
+
+
+	echo "<table>";
+	echo "<tr><th>Description</th><th>Quantity</th><th>Cost</th></tr>";
+	$bookingtotal = 0;
+
+	$playerbooking = 0;
+
+foreach($_POST as $itemname => $value)
+{
+	$itemname = htmlentities(stripslashes($itemname));
+	$value = htmlentities(stripslashes($value));
+
+	if (strpos($itemname, 'chk') !== false || strpos($itemname, 'cbo') !== false)
+	{
+		$itemid = str_replace('chk', '', $itemname);
+		$itemid = str_replace('cbo', '', $itemid);
+		$itemid = (int)$itemid;
+		$value = str_replace('on', '1', $value);
+
+		if ($itemid > 0)
+		{
+			$sql = "select * from {$db_prefix}items where itItemID = $itemid";
+			$result = ba_db_query ($link, $sql);
+			$iteminfo = ba_db_fetch_assoc ($result);
+
+			if ($value > 0)
+			{
+				echo "<tr><td>".$iteminfo['itDescription']."</td><td>$value</td><td>&pound;".$iteminfo['itItemCost']."</td></tr>";
+				$bookingtotal += ($iteminfo['itItemCost'] * $value);
+
+				//Create item record
+				$sql = "INSERT INTO {$db_prefix}bookingitems (biBookingID, biItemID, biQuantity) VALUES (".$bookinginfo['bkID'].", $itemid, $value)";
+
+				ba_db_query ($link, $sql);
+				//Update bookinginfo
+				if ($iteminfo['itBunk']) { $bookinginfo['bkBunkRequested'] = 1; }
+				if ($iteminfo['itMeal']) { $bookinginfo['bkMealTicket'] = 1; }
+
+				if ($iteminfo['itAvailability'] == "Player") { $playerbooking = 1; }
+			}
+		}
+
+	}
+
+}
+	$bookingtotal = number_format($bookingtotal, 2);
+	$bookinginfo['bkAmountExpected'] = $bookingtotal;
+
+	echo "<tr class='total'><td colspan=2 class='totallabel'>Total:</td><td>&pound;$bookingtotal</span></td></tr>";
+	echo "</table>";
+
+	//Deal with booking queue people
+	$bookinginfo['bkInQueue'] = 0;
+	if ($eventinfo['evUseQueue'])
+	{
+		$sql = "select chFaction from {$db_prefix}characters where chPlayerID = $PLAYER_ID";
+		$result = ba_db_query ($link, $sql);
+		$characterinfo = ba_db_fetch_assoc ($result);
+		if ($playerbooking == 1 && $characterinfo['chFaction'] != DEFAULT_FACTION)
+		{
+			$bookinginfo['bkInQueue'] = 1;
+			$queuereason = "your character is not a member of the default faction.";
+		}
+
+		//Deal with being over the limit
+		if (QUEUE_OVER_LIMIT)
+		{
+			if ($bookinginfo['bkBookAs'] == "Player") { $spaces = $eventinfo['evPlayerSpaces']; }
+			if ($bookinginfo['bkBookAs'] == "Monster") { $spaces = $eventinfo['evMonsterSpaces']; }
+			if ($bookinginfo['bkBookAs'] == "Staff") { $spaces = $eventinfo['evStaffSpaces']; }
+			$limitsql = "select count(bkID) as BookingCount from {$db_prefix}bookings where bkInQueue = 0 and bkBookAs ='".$bookinginfo['bkBookAs']."' and bkEventID = $eventid ";
+			$limitresult = ba_db_query ($link, $limitsql);
+			$BookingCount = ba_db_fetch_assoc ($limitresult);
+			$BookingCount = $BookingCount['BookingCount'];
+			if ($BookingCount > $spaces)
+			{
+				$bookinginfo['bkInQueue'] = 1;
+				$queuereason = "there are no spaces remaining of your booking type.";
+			}
+		}
+	}
+
+	if ($bookinginfo['bkInQueue'] == 0)
+	{
+		if ($bookingtotal > 0)
+		{
+			echo "<table class='payment'>";
+			echo "<tr><td>Pay Later</td><td><a href='start.php'>Pay later</a></td></tr>";
+			if (USE_PAY_PAL) {
+				echo "<tr><td>Pay balance of &pound;$bookingtotal via Paypal:</td><td>";
+				generatePaypalButton("Event booking - ".$bookinginfo['evEventName'] . " (" . PID_PREFIX . sprintf ('%03s', $PLAYER_ID).")", $PLAYER_ID, $bookingtotal, $bookinginfo['bkID']);
+				echo "</td></tr>";
+				echo "<tr><td>Pay another amount:</td><td><input type='text' value='".$bookingtotal."' onChange='updatePaypalButton()' id='txtAnotherAmount' class='paypalamount'/>";
+				generatePaypalButton("Event booking - ".$bookinginfo['evEventName'] . " (" . PID_PREFIX . sprintf ('%03s', $PLAYER_ID).") - Partial Payment", $PLAYER_ID, $bookingtotal, $bookinginfo['bkID'], 'anotheramount');
+				echo "</td></tr>";
+			}
+			echo "<tr><td>Request payment by another user:</td><td>";
+			echo "<form action='start.php' method='post'><input type=hidden name='hBooking' value='".$bookinginfo['bkID']."' /><input type=text name='txtEmail'/><input type='submit' name='btnSubmit' value='Send Email' /></form>";
+			echo "</td></tr>";
+			echo "</table>";
+
+		}
+		else
+		{
+			echo "<p>Your booking is now complete, as it does not require any payment</p>";
+			$bookinginfo['bkDatePaymentConfirmed'] = $today;
+		}
+
+	}
+	else
+	{
+		echo "<p>Your booking has been placed in a queue because $queuereason</p>";
+		echo "<p>You will be able to pay once it is removed from the queue by an admin.</p>";
+		fnMailer ("Event booking - ".$eventinfo['evEventName'] . " (" . PID_PREFIX . sprintf ('%03s', $PLAYER_ID).")\nA booking has been placed in the queue", false);
+	}
+
+	//Save booking info
+	$sql = "UPDATE {$db_prefix}bookings SET ";
+	foreach ($bookinginfo as $key => $value)
+	{
+		if (!is_numeric($value)) {$value = "'".$value."'"; }
+		$sql.= "$key = $value,";
+	}
+	$sql = substr($sql,0,-1);
+	$sql.= " WHERE bkID = ".$bookinginfo['bkID'];
+
+	$result = ba_db_query ($link, $sql);
+
+include ('inc/inc_foot.php');
+?>
--- eventdetails.php
+++ eventdetails.php
@@ -1,1 +1,150 @@
-<?php/*Bitsand - a web-based booking system for LRP eventsCopyright (C) 2006 - 2012 The Bitsand Project (http://bitsand.googlecode.com/)This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/$bLoginCheck = False;include ('inc/inc_head_db.php');include ('inc/inc_commonqueries.php');$db_prefix = DB_PREFIX;$eventinfo = getEventDetails($_GET['EventID'], 0);$metadescription = "Online booking for ".htmlentities (stripslashes ($eventinfo['evEventName']));include ('inc/inc_head_html.php');?><script src="inc/sorttable.js" type="text/javascript"></script><h2>Event Details - <?php echo htmlentities (stripslashes ($eventinfo['evEventName'])); ?></h2><?phpif ((BOOKING_LIST_IF_LOGGED_IN && $PLAYER_ID != 0) || !BOOKING_LIST_IF_LOGGED_IN) { echo "<p><a href='#booked'>View booked players</a></p>"; }echo "<div class='eventdescription'>\n";echo stripslashes ($eventinfo['evEventDescription']);echo "</div>\n<div class='eventdetails'>\n";echo stripslashes ($eventinfo['evEventDetails']);echo "</div>";//Get list of players that have paid$sql = "SELECT plFirstName, " .	"plSurname, " .	"bkBookAs, " .	"chName, chPreferredName, chGroupSel, chGroupText, chFaction, chMonsterOnly, " .	"bkDatePaymentConfirmed " .	"FROM {$db_prefix}players INNER JOIN {$db_prefix}bookings ON plPlayerID = bkPlayerID " .	"LEFT OUTER JOIN {$db_prefix}characters ON plPlayerID = chPlayerID " .	"WHERE bkDatePaymentConfirmed <> '' " .	"AND bkDatePaymentConfirmed <> '0000-00-00' " .	"AND bkEventID = ". $eventinfo['evEventID'];$result = ba_db_query ($link, $sql);if ((BOOKING_LIST_IF_LOGGED_IN && $PLAYER_ID != 0) || !BOOKING_LIST_IF_LOGGED_IN) {	echo "<a name='booked' />";	echo "<p>The people below have booked and paid for this event</p>";	echo "<table border = '1' class='sortable'><tr><th>OOC First Name</th><th>OOC Surname</th><th>IC Name</th>";	if (LIST_GROUPS_LABEL != '')		echo "<th>Group</th>";	echo "<th>Faction</th>";	echo "<th>Booking As</th></tr>\n";	while ($row = ba_db_fetch_assoc ($result)) {		echo "<tr class = 'highlight'>\n";		echo "<td>" . htmlentities (stripslashes ($row ['plFirstName'])) . "</td>\n";		echo "<td>" . htmlentities (stripslashes ($row ['plSurname'])) . "</td>\n";		if ($row['chMonsterOnly'] == 0)		{			if (strlen($row ['chPreferredName']) == 0)			{				echo "<td>" . htmlentities (stripslashes ($row ['chName'])) . "</td>\n";			}			else			{				echo "<td>" . htmlentities (stripslashes ($row ['chPreferredName'])) . "</td>\n";			}			if (LIST_GROUPS_LABEL != '') {				echo "<td>\n";				if ($row ['chGroupText'] == 'Enter name here if not in above list' || $row ['chGroupText'] == '')					echo htmlentities (stripslashes ($row ['chGroupSel']));				else					echo "Other (" . htmlentities (stripslashes ($row ['chGroupText'])) . ")";				echo "</td>\n";			}			echo "<td>". htmlentities (stripslashes ($row ['chFaction'])) ."</td>";		}		else {			echo "<td></td>";			if (LIST_GROUPS_LABEL != '') {				echo "<td></td>";			}			echo "<td></td>";		}		$bookingtype = str_replace("Staff", $stafftext, $row ['bkBookAs']);		echo "<td>" . htmlentities (stripslashes ($bookingtype)) . "</td>\n</tr>\n";	}	echo "</table>";	if ($eventinfo['evAllowMonsterBookings']) {		$sql = "SELECT plPlayerID, " .			"bkBookAs, " .			"bkDatePaymentConfirmed " .			"FROM {$db_prefix}players, {$db_prefix}bookings " .			"WHERE bkBookAs LIKE 'Monster' AND plPlayerID = bkPlayerID AND bkDatePaymentConfirmed <> '' AND bkDatePaymentConfirmed <> '0000-00-00' AND bkEventID = ". $eventinfo['evEventID'];		$result = ba_db_query ($link, $sql);		$iMonsters = ba_db_num_rows ($result);	}	else		$iMonsters = 0;	$sql = "SELECT plPlayerID, " .		"bkBookAs, " .		"bkDatePaymentConfirmed " .		"FROM {$db_prefix}players, {$db_prefix}bookings " .		"WHERE bkBookAs LIKE 'Player' AND plPlayerID = bkPlayerID AND bkDatePaymentConfirmed <> '' AND bkDatePaymentConfirmed <> '0000-00-00' AND bkEventID = ". $eventinfo['evEventID'];	$result = ba_db_query ($link, $sql);	$iPlayers = ba_db_num_rows ($result);	$sql = "SELECT plPlayerID, " .		"bkBookAs, " .		"bkDatePaymentConfirmed " .		"FROM {$db_prefix}players, {$db_prefix}bookings " .		"WHERE bkBookAs LIKE 'Staff' AND plPlayerID = bkPlayerID AND bkDatePaymentConfirmed <> '' AND bkDatePaymentConfirmed <> '0000-00-00' AND bkEventID = ". $eventinfo['evEventID'];	$result = ba_db_query ($link, $sql);	$iStaff = ba_db_num_rows ($result);	$iTotal = $iMonsters + $iPlayers + $iStaff;	$iCrew = $iMonsters + $iStaff;	echo "<p>\n";	echo "$iStaff $stafftext, ";	if ($eventinfo['evAllowMonsterBookings'])		echo "$iMonsters Monsters, ";	echo "$iPlayers Players. ($iTotal total)\n</p>";	echo "<p class = 'smallprint'><a href = 'bookings_rss.php?event={$eventinfo['evEventID']}'>RSS feed of bookings for this event</a></p>\n";}else {	echo "<p>The list of bookings for this event is only available if you are logged into the system.</p>";}echo "<p class = 'smallprint'><a href='iCalendar.php'>iCalendar feed of events</a></p>\n";include ('inc/inc_foot.php');?>
+<?php
+/*
+Bitsand - a web-based booking system for LRP events
+Copyright (C) 2006 - 2012 The Bitsand Project (http://bitsand.googlecode.com/)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+$bLoginCheck = False;
+
+
+include ('inc/inc_head_db.php');
+include ('inc/inc_commonqueries.php');
+
+$db_prefix = DB_PREFIX;
+$eventinfo = getEventDetails($_GET['EventID'], 0);
+$metadescription = "Online booking for ".htmlentities (stripslashes ($eventinfo['evEventName']));
+include ('inc/inc_head_html.php');
+
+
+?>
+<script src="inc/sorttable.js" type="text/javascript"></script>
+<h2>Event Details - <?php echo htmlentities (stripslashes ($eventinfo['evEventName'])); ?></h2>
+
+<?php
+if ((BOOKING_LIST_IF_LOGGED_IN && $PLAYER_ID != 0) || !BOOKING_LIST_IF_LOGGED_IN) { echo "<p><a href='#booked'>View booked players</a></p>"; }
+echo "<div class='eventdescription'>\n";
+echo stripslashes ($eventinfo['evEventDescription']);
+echo "</div>\n<div class='eventdetails'>\n";
+echo stripslashes ($eventinfo['evEventDetails']);
+echo "</div>";
+
+
+//Get list of players that have paid
+$sql = "SELECT plFirstName, " .
+	"plSurname, " .
+	"bkBookAs, " .
+	"chName, chPreferredName, chGroupSel, chGroupText, chFaction, chMonsterOnly, " .
+	"bkDatePaymentConfirmed " .
+	"FROM {$db_prefix}players INNER JOIN {$db_prefix}bookings ON plPlayerID = bkPlayerID " .
+	"LEFT OUTER JOIN {$db_prefix}characters ON plPlayerID = chPlayerID " .
+	"WHERE bkDatePaymentConfirmed <> '' " .
+	"AND bkDatePaymentConfirmed <> '0000-00-00' " .
+	"AND bkEventID = ". $eventinfo['evEventID'];
+
+$result = ba_db_query ($link, $sql);
+
+if ((BOOKING_LIST_IF_LOGGED_IN && $PLAYER_ID != 0) || !BOOKING_LIST_IF_LOGGED_IN) {
+	echo "<a name='booked' />";
+	echo "<p>The people below have booked and paid for this event</p>";
+	echo "<table border = '1' class='sortable'><tr><th>OOC First Name</th><th>OOC Surname</th><th>IC Name</th>";
+
+	if (LIST_GROUPS_LABEL != '')
+		echo "<th>Group</th>";
+
+	echo "<th>Faction</th>";
+	echo "<th>Booking As</th></tr>\n";
+
+	while ($row = ba_db_fetch_assoc ($result)) {
+		echo "<tr class = 'highlight'>\n";
+		echo "<td>" . htmlentities (stripslashes ($row ['plFirstName'])) . "</td>\n";
+		echo "<td>" . htmlentities (stripslashes ($row ['plSurname'])) . "</td>\n";
+
+		if ($row['chMonsterOnly'] == 0)
+		{
+			if (strlen($row ['chPreferredName']) == 0)
+			{
+				echo "<td>" . htmlentities (stripslashes ($row ['chName'])) . "</td>\n";
+			}
+			else
+			{
+				echo "<td>" . htmlentities (stripslashes ($row ['chPreferredName'])) . "</td>\n";
+			}
+			if (LIST_GROUPS_LABEL != '') {
+				echo "<td>\n";
+				if ($row ['chGroupText'] == 'Enter name here if not in above list' || $row ['chGroupText'] == '')
+					echo htmlentities (stripslashes ($row ['chGroupSel']));
+				else
+					echo "Other (" . htmlentities (stripslashes ($row ['chGroupText'])) . ")";
+				echo "</td>\n";
+			}
+
+			echo "<td>". htmlentities (stripslashes ($row ['chFaction'])) ."</td>";
+
+		}
+		else {
+			echo "<td></td>";
+			if (LIST_GROUPS_LABEL != '') {
+				echo "<td></td>";
+			}
+			echo "<td></td>";
+		}
+		$bookingtype = str_replace("Staff", $stafftext, $row ['bkBookAs']);
+		echo "<td>" . htmlentities (stripslashes ($bookingtype)) . "</td>\n</tr>\n";
+	}
+
+	echo "</table>";
+
+	if ($eventinfo['evAllowMonsterBookings']) {
+		$sql = "SELECT plPlayerID, " .
+			"bkBookAs, " .
+			"bkDatePaymentConfirmed " .
+			"FROM {$db_prefix}players, {$db_prefix}bookings " .
+			"WHERE bkBookAs LIKE 'Monster' AND plPlayerID = bkPlayerID AND bkDatePaymentConfirmed <> '' AND bkDatePaymentConfirmed <> '0000-00-00' AND bkEventID = ". $eventinfo['evEventID'];
+		$result = ba_db_query ($link, $sql);
+		$iMonsters = ba_db_num_rows ($result);
+	}
+	else
+		$iMonsters = 0;
+	$sql = "SELECT plPlayerID, " .
+		"bkBookAs, " .
+		"bkDatePaymentConfirmed " .
+		"FROM {$db_prefix}players, {$db_prefix}bookings " .
+		"WHERE bkBookAs LIKE 'Player' AND plPlayerID = bkPlayerID AND bkDatePaymentConfirmed <> '' AND bkDatePaymentConfirmed <> '0000-00-00' AND bkEventID = ". $eventinfo['evEventID'];
+	$result = ba_db_query ($link, $sql);
+	$iPlayers = ba_db_num_rows ($result);
+	$sql = "SELECT plPlayerID, " .
+		"bkBookAs, " .
+		"bkDatePaymentConfirmed " .
+		"FROM {$db_prefix}players, {$db_prefix}bookings " .
+		"WHERE bkBookAs LIKE 'Staff' AND plPlayerID = bkPlayerID AND bkDatePaymentConfirmed <> '' AND bkDatePaymentConfirmed <> '0000-00-00' AND bkEventID = ". $eventinfo['evEventID'];
+	$result = ba_db_query ($link, $sql);
+	$iStaff = ba_db_num_rows ($result);
+	$iTotal = $iMonsters + $iPlayers + $iStaff;
+	$iCrew = $iMonsters + $iStaff;
+	echo "<p>\n";
+	echo "$iStaff $stafftext, ";
+	if ($eventinfo['evAllowMonsterBookings'])
+		echo "$iMonsters Monsters, ";
+	echo "$iPlayers Players. ($iTotal total)\n</p>";
+	echo "<p class = 'smallprint'><a href = 'bookings_rss.php?event={$eventinfo['evEventID']}'>RSS feed of bookings for this event</a></p>\n";
+}
+else {
+	echo "<p>The list of bookings for this event is only available if you are logged into the system.</p>";
+}
+
+echo "<p class = 'smallprint'><a href='iCalendar.php'>iCalendar feed of events</a></p>\n";
+include ('inc/inc_foot.php');
+?>
