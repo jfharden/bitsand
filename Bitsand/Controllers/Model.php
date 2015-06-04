4165371f2dd0d800f445776c74984167eafce056
Pete Allison <pete.allison@gompels.co.uk>
2015-06-04 17:20:58 +0100

    Added Model Controller master

---
+++ Bitsand/Controllers/Model.php
@@ -0,0 +1,47 @@
+<?php
+/*+----------------------------------------------------------------------------
+ || Bitsand - an online booking system for Live Role Play events
+ ||
+ || File Bitsand/Controllers/Model.php
+ ||    Summary: The base Model class from where all models originate
+ ||             from.
+ ||
+ ||     Author: Pete Allison
+ ||  Copyright: (C) 2006 - 2015 The Bitsand Project
+ ||             (http://github.com/PeteAUK/bitsand)
+ ||
+ || Bitsand is free software; you can redistribute it and/or modify it under the
+ || terms of the GNU General Public License as published by the Free Software
+ || Foundation, either version 3 of the License, or (at your option) any later
+ || version.
+ ||
+ || Bitsand is distributed in the hope that it will be useful, but WITHOUT ANY
+ || WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ || FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ || details.
+ ||
+ || You should have received a copy of the GNU General Public License along with
+ || Bitsand.  If not, see <http://www.gnu.org/licenses/>.
+ ++--------------------------------------------------------------------------*/
+
+namespace Bitsand\Controllers;
+
+use Bitsand\Registry;
+
+abstract class Model {
+	protected $db;
+
+	public function __construct() {
+		$this->db = Registry::get('database');
+	}
+
+	public function __get($key) {
+		return Registry::get($key);
+	}
+
+	public function __set($key, $value) {
+		// Not right
+		die('something not right in setting');
+		$this->registry->set($key, $value);
+	}
+}
