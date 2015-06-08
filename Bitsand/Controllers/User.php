fda00cb4ffb56ac2eced3d031bcb312ebe69e1f6
Pete Allison <pete.allison@gompels.co.uk>
2015-06-08 17:01:27 +0100

    Documented User and added getName

--- Bitsand/Controllers/User.php
+++ Bitsand/Controllers/User.php
@@ -32,27 +32,52 @@ class User {
 
 	private $is_admin = false;
 
+	private $name;
+
+	protected $session;
+
 	public function __construct() {
+		$this->session = Registry::get('session');
+
 		if (isset($this->session->data['user_id'])) {
 			$this->user_id = (int)$this->session->data['user_id'];
 		}
 	}
 
+	/**
+	 * Checks to see if the user is logged in
+	 * @return boolean
+	 */
 	public function isLogged() {
 		return $this->user_id !== null;
 	}
 
+	/**
+	 * Checks to see if the current user has admin rights
+	 * @return boolean
+	 */
 	public function isAdmin() {
 		return $this->is_admin;
 	}
 
+	/**
+	 * Logs a user in, setting the passed data in the session.
+	 * $data must contain a user_id field
+	 * @param array $data
+	 */
 	public function logIn($data) {
+		if (!isset($data['user_id'])) {
+			throw new \Bitsand\Exceptions\NoUserIDSetException('No user_id key set in user logIn');
+		}
 		foreach ($data as $key => $value) {
 			$this->session->data[$key] = $value;
 		}
 		$this->session->data['keys'] = array_keys($data);
 	}
 
+	/**
+	 * Logs the user out and unsets the various keys within the session
+	 */
 	public function logOut() {
 		if (isset($this->session->data['keys'])) {
 			foreach ($this->session->data['keys'] as $key) {
@@ -60,5 +85,19 @@ class User {
 			}
 			unset($this->session->data['keys']);
 		}
+	}
+
+	/**
+	 * Returns the logged in user's name
+	 * @return string
+	 */
+	public function getName() {
+		if (!is_null($this->name)) {
+			$name = (isset($this->session->data['firstname']) ? $this->session->data['name'] : '') . ' ' . (isset($this->session->data['lastname']) ? $this->session->data['name'] : '');
+
+			$this->name = trim($name);
+		}
+
+		return $this->name;
 	}
 }
