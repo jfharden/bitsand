0fa2799bac178026b720cc9440d713b7480e0646
Pete Allison <pete.allison@gompels.co.uk>
2015-06-10 14:23:12 +0100

    Fixed issue where sessions weren't saving prior to a redirect

--- Bitsand/Controllers/Controller.php
+++ Bitsand/Controllers/Controller.php
@@ -57,6 +57,8 @@ abstract class Controller {
 	protected function redirect($url, $status = 302) {
 		header('Status: ' . $status);
 		header('Location: ' . str_replace(array('&amp;', "\n", "\r"), array('&', '', ''), $url));
+		// We MUST exit() after a redirection, this ensures the session is correctly saved
+		exit();
 	}
 
 	protected function getTemplate($view_route) {
